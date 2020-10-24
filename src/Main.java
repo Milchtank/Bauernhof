46429ccc7b05b25dedc6ce22092f37d5c4a1bba3
Milchtank <fabripiac@gmail.com>
2020-10-24 12:53:39 +0200

    Adding olga

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		Kuh olga = new Kuh("olga", 200, 24, "blau");
+				
+			olga.essen();	
 	}
 
 }
